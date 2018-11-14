ad92f3de375876868b05fddf6a7178d06fb1c933
Patricia Artis <patricaartis@gmail.com>
2018-11-14 23:15:50 +0000

    math random

--- js/script.js
+++ js/script.js
@@ -9,3 +9,14 @@
 
 // DOCUMENT READY FUNCTION BELOW
 
+$("#shoot").click(function(){
+    var randomNumber = Math.random();
+     if(randomNumber <0.3333 ){
+        $("#computerChoice").text("rock"); 
+         } else if(randomNumber < 0.6666 && randomNumber > 0.3333 ){
+        $("#computerChoice").text("paper"); 
+         } else if(randomNumber > 0.6666 ){
+        $("#computerChoice").text("scissor"); 
+         };
+    
+});
